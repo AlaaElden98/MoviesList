0a3a0203fd0235c2a9573726ae4b2c27fdca529d
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-05-25 17:20:11 +0200

    [Git] Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {  
+  return (
+    <SafeAreaView>
+      <Text>React-native app</Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
